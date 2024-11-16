0431f8fa3c01d00b88c671eaef6e26cbcb322171
DeltaDynamo <159301654+DeltaDynamo@users.noreply.github.com>
2024-11-16 13:10:04 +0530

    Add Request Model, Repo & Services

---
+++ packages/app/repositories/Request.js
@@ -0,0 +1,15 @@
+const BaseRepository = require("../repositories/base");
+const Request = require("../models/Request");
+
+/**
+ * The Request Repository extends BaseRepository to manage Request model operations, inheriting CRUD methods like getById, getAll, create, update, and delete..
+ * It passes the Request model to the base repository for database interactions.
+ *  Custom methods specific to Request can also be added as needed.
+*/
+class RequestRepository extends BaseRepository {
+  constructor() {
+    super(Request);
+  }
+}
+
+module.exports = RequestRepository;
