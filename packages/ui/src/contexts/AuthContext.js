ecbf9d0fc29ba725a1bc780c8e32b6de242e65c8
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2024-11-22 22:04:09 +0530

    Implemented protected routes logic along with User and Auth context

---
+++ packages/ui/src/contexts/AuthContext.js
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import { instance } from "../api/api_instance";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthCheckComplete, setAuthCheckComplete] = useState(false);
+
+  useEffect(() => {
+    const checkCookieExists = () => {
+      const jwtCookie = document.cookie
+        .split(";")
+        .some((item) => item.trim().startsWith("jwt="));
+      setIsAuthenticated(jwtCookie);
+      setAuthCheckComplete(true);
+    };
+
+    checkCookieExists();
+  }, []);
+
+  const logout = async () => {
+    try {
+      await instance.get(`api/auth/signout`);
+      setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, logout }}
+    >
+      {isAuthCheckComplete ? children : null}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
