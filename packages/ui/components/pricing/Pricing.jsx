9b44ca814ad135684e9d5b3d3422efa2d4df87a3
Hariom Vashista <rishivashista4@gmail.com>
2024-11-23 15:46:45 +0530

    Move constant to util folder and removed eslint error

--- packages/ui/components/pricing/Pricing.jsx
+++ packages/ui/components/pricing/Pricing.jsx
@@ -1,52 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
 import styles from "./Pricing.module.css"
 import PricingCard from './PricingCard'
+import plans from '../../utils/constants'
 
 function Pricing() {
-
-  let plans = [
-    {
-      'index': 0,
-      'name': 'Basic plan',
-      'pricing': 10,
-      'tagline': 'Ideal for small teams and startups.',
-      'keypoints': [
-        'Fortune 500 company logo',
-        '500 API calls per month',
-        '2 API keys',
-        'Basic analytics',
-        '48-72 hour of response time'
-      ]
-    },
-    {
-      'index': 1,
-      'name': 'Business plan',
-      'pricing': 20,
-      'tagline': 'Growing teams up to 20 users.',
-      'keypoints': [
-        'Fortune 500 company logo + private images',
-        '10000 API calls per month',
-        '5 API keys',
-        'Advance analytics',
-        '12-36 hours of response time'
-      ]
-    },
-    {
-      'index': 2,
-      'name': 'Enterprise plan',
-      'pricing': 40,
-      'tagline': 'Large teams with unlimited users.',
-      'keypoints': [
-        'Fortune 500 company logo + unlimited private logos',
-        'Unlimited API calls per month',
-        '50 API keys',
-        'Advance analytics',
-        'Priority support'
-      ]
-    }
-  ]
-
   return (
     <div className={styles.mainDiv}>
       <div>
--- packages/ui/components/pricing/PricingCard.jsx
+++ packages/ui/components/pricing/PricingCard.jsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
 import PropTypes from 'prop-types'
 import styles from "./PricingCard.module.css"
 import tickIcon from '../../src/assets/Icon.svg'
