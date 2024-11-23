8f256ab8e6ca65eedc0fddfac6e1f2f1b1a4a573
DeltaDynamo <159301654+DeltaDynamo@users.noreply.github.com>
2024-11-23 13:57:42 +0530

    feat: Add signin controller and update route

--- packages/app/routes/auth.js
+++ packages/app/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const signupController = require("../controllers/auth/signup");
+const signinController = require("../controllers/auth/signin");
 
 router.post("/signup", signupController);
+router.post("/signin", signinController);
 
 module.exports = router;
