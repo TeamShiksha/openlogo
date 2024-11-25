962201bb552036ecfd6593086e9db475f175b8cc
Abhishek <asharma991@outlook.com>
2024-11-26 02:34:04 +0530

    feat: add hero section

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Demo from "./components/demo/Demo";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
       <Demo />
     </>
   );
