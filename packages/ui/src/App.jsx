41b5044659ea6e49e56cbf1d707b86274d921e3f
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2024-12-01 15:27:56 +0530

    fix: fixed /about route in app router

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/about" element={<Navigate to="/about" />} />
+        <Route path="/about" element={<About />} />
         <Route
           path="/admin"
           element={
