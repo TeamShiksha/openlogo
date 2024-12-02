78413ca62b7eed86f13cf604e469af9d8441b395
shantanu pawar <shantanupawar101@gmail.com>
2024-12-02 18:32:57 +0530

    Fix: Replaced .css with module.css and removed test files

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Demo from "./components/demo/Demo";
-
+import Dashboard from "./Pages/dashboard/Dashboard";
 function App() {
-
   return (
     <>
-      <Demo/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Demo />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- packages/ui/src/Pages/dashboard/Dashboard.jsx
+++ packages/ui/src/Pages/dashboard/Dashboard.jsx
@@ -1,17 +1,17 @@
-import ApiKeyForm from '../../components/dashboard/ApiKeyForm';
-import CurrentPlan from '../../components/dashboard/CurrentPlan';
-import Usage from '../../components/dashboard/Usage';
-import './Dashboard.css';
+import ApiKeyForm from '../../components/dashboard/ApiKeyForm/ApiKeyForm';
+import CurrentPlan from '../../components/dashboard/CurrentPlan/CurrentPlan';
+import Usage from '../../components/dashboard/Usage/Usage'
+import styles from './Dashboard.module.css';
 
 function Dashboard() {
 
 	return (
-		<div className='dashboard-container' data-testid='testid-dashboard'>
-			<div className='dashboard-content-container'>
-				<section className='dashboard-content-section'>
+		<div className={styles.dashboardContainer} data-testid='testid-dashboard'>
+			<div className={styles.dashboardContentContainer}>
+				<section className={styles.dashboardContentSection}>
 					<Usage />
-					<div className='generate-api'>
-						<h1 className='content-item-heading'>Generate New API key</h1>
+					<div className={styles.generateApi}>
+						<h1 className={styles.contentItemHeading}>Generate New API key</h1>
 						<ApiKeyForm/>
 					</div>
 					<CurrentPlan />
