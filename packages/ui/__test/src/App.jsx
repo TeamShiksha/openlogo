3468c2d8a5763899999f1d133fe0900c37c00009
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-04 23:31:03 +0530

    only added the test files respective to the folders

---
+++ packages/ui/__test/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import AdminDashboard from "./pages/admin/Admin";
+import About from "./pages/about/About";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import Home from "./pages/home/Home";
+import "./App.css";
+import Dashboard from "./Pages/dashboard/Dashboard";
+import Pricing from "../components/pricing/Pricing";
+import Footer from "./components/footer/Footer";
+import PrivacyPolicy from "./pages/privacypolicy/PrivacyPolicy";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              <AdminDashboard />
+            </ProtectedRoute> 
+          }
+        />
+      </Routes>
+  
+      <Footer />
+    </div>
+     
+  );
+}
+export default App;
