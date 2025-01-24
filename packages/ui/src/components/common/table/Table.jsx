798eb52e73f20484c91db306abb77b237c6451ee
ayushsanjdev <ayushsanjpro@gmail.com>
2025-01-25 00:04:02 +0530

    updated nodata message

--- packages/ui/src/components/common/table/Table.jsx
+++ packages/ui/src/components/common/table/Table.jsx
@@ -1,46 +1,8 @@
-/**
- * Table Component
- *
- * Props:
- * - headers: Array of objects representing table headers.
- *   Each header object must have:
- *     - id (string): A unique identifier for the header.
- *     - label (string): The text to display in the header.
- *   Example:
- *   headers = [
-  { id: "name", label: "Name" },
-  { id: "age", label: "Age" },
-];
- *
- * - rows: Array of objects representing table rows.
- *   Each row object must have:
- *     - id (string): A unique identifier for the row.
- *     - cells (array): An array of objects representing the cells in the row.
- *       Each cell object must have:
- *         - id (string): A unique identifier for the cell.
- *         - value (any): The content to display in the cell.
- *   Example:
- *   rows = [
-  {
-    id: "row1",
-    cells: [
-      { id: "cell1", value: "John" },
-      { id: "cell2", value: 25 },
-    ],
-  },
-  {
-    id: "row2",
-    cells: [
-      { id: "cell5", value: "Jane" },
-      { id: "cell6", value: 30 },
-    ],
-  },
-];
- */
-
 import classes from "./Table.module.css";
 
-const Table = ({ headers, rows }) => {
+const Table = ({ headers, rows, emptyMessage }) => {
+  const hasData = rows && rows?.length > 0;
+
   return (
     <table className={classes["custom-table"]}>
       <thead>
@@ -53,15 +15,23 @@ const Table = ({ headers, rows }) => {
         </tr>
       </thead>
       <tbody>
-        {rows.map(({ id, cells }) => (
-          <tr key={id}>
-            {cells.map(({ id, value }) => (
-              <td key={id} className={classes["table-cell"]}>
-                {value}
-              </td>
-            ))}
+        {hasData ? (
+          rows.map(({ id, cells }) => (
+            <tr key={id}>
+              {cells.map(({ id, value }) => (
+                <td key={id} className={classes["table-cell"]}>
+                  {value}
+                </td>
+              ))}
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={headers?.length} className={classes["no-data"]}>
+              {emptyMessage || "No data to display"}
+            </td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   );
