88241e401d091c3452d848e9926c0c379ded03a9
Shafat <shafathussain933@gmail.com>
2025-01-27 23:39:46 +0530

    herosectiontest

---
+++ packages/ui/__tests__/HeroSection.test.jsx
@@ -0,0 +1,49 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import HeroSection from "../src/components/hero/HeroSection";
+import { expect, it } from "vitest";
+
+describe("HeroSection Component", () => {
+  it("renders the HeroSection with heading and description", () => {
+    render(<HeroSection />);
+    expect(
+      screen.getByText("Access hundreds of logos with just one line of code")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "A collection of APIs designed to simplify the process of obtaining company logos. Generate API keys in seconds."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("renders the buttons correctly", () => {
+    render(<HeroSection />);
+
+    expect(screen.getByText("Documentation")).toBeInTheDocument();
+    expect(screen.getByText("Get started")).toBeInTheDocument();
+  });
+
+  it("renders the logo image in the right section", () => {
+    render(<HeroSection />);
+    const logoImage = screen.getByAltText("Logo Images");
+    expect(logoImage).toBeInTheDocument();
+    expect(logoImage).toHaveAttribute("src", "logo-images.png");
+  });
+
+  it("does not render the modal initially", () => {
+    render(<HeroSection />);
+    expect(screen.queryByText("AuthModal")).not.toBeInTheDocument();
+  });
+
+  it("opens the modal when the 'Get started' button is clicked", () => {
+    render(<HeroSection />);
+    fireEvent.click(screen.getByText("Get started"));
+    expect(screen.queryByText("AuthModal")).toBeInTheDocument();
+  });
+
+  it("closes the modal when the 'Close' button is clicked", () => {
+    render(<HeroSection />);
+    fireEvent.click(screen.getByText("Get started"));
+    fireEvent.click(screen.getByText("signupModal"));
+    expect(screen.queryByText("AuthModal")).not.toBeInTheDocument();
+  });
+});
