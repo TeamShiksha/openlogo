c3068d5e748895e659773be710117260085094d8
mridxl <mridxl03@gmail.com>
2025-02-28 03:39:43 +0530

    fix: update signout response and make route parameters optional for verification and password reset

--- packages/app/routes/auth.js
+++ packages/app/routes/auth.js
@@ -12,9 +12,9 @@ const {
 router.post("/signup", signupController);
 router.post("/signin", signinController);
 router.post("/signout", signoutController);
-router.get("/verify/:token", verifyEmailController);
+router.get("/verify/:token?", verifyEmailController);
 router.post("/password/forgot", forgotPasswordController);
-router.get("/password/forgot/:token", resetPasswordSessionController);
+router.get("/password/forgot/:token?", resetPasswordSessionController);
 router.patch("/password/reset", resetPasswordController);
 
 module.exports = router;
