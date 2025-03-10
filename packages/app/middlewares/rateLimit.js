ceec08257ad87db7b8a4c4d9bce035b9f7b4438e
farhan294sha <farhanshabeer294@gmail.com>
2025-03-10 19:14:02 +0530

    added comments

--- packages/app/middlewares/rateLimit.js
+++ packages/app/middlewares/rateLimit.js
@@ -1,9 +1,9 @@
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minutes
+  windowMs: 60 * 1000, // 1 minute
   limit: 100,
-  standardHeaders: "draft-8",
+  standardHeaders: "draft-8", //  eigth draft of the IETF rate limit header specification
   legacyHeaders: false,
 });
 
