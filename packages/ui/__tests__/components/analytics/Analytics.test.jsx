34e3909e1444899b89fc0481b0030a9e41e6d612
Arya Dharkar <142116758+AryaDharkar@users.noreply.github.com>
2025-06-06 22:28:35 +0530

    Integrate stats API on frontend, fix: backend JSON response structure (#697)
    * fix: backend JSON response structure, integrate API on frontend
    
    * fix: update documentation to add stats api endpoint
    
    * fix: api calling method, test
    
    * Update AnalyticsCard.jsx
    
    ---------
    
    Co-authored-by: Anoop Pratap Singh <anoopprsingh.social@gmail.com>

--- packages/ui/__tests__/components/analytics/Analytics.test.jsx
+++ packages/ui/__tests__/components/analytics/Analytics.test.jsx
@@ -1,18 +1,35 @@
-import { render, screen } from "@testing-library/react";
-import { expect, describe, it } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import { expect, describe, it, vi, afterEach } from "vitest";
 import Analytics from "../../../src/components/analytics/Analytics";
-import { ANALYTIC_CARDS } from "../../../src/utils/Constants";
+import { instance } from "../../../src/api/api_instance";
+
+const mockData = [
+  { title: "Users", value: 10 },
+  { title: "Keys", value: 20 },
+  { title: "Requests", value: 40 },
+  { title: "Hits", value: 40 },
+];
+
+vi.mock("../../../src/api/api_instance", () => ({
+  instance: vi.fn(() => Promise.resolve({ data: { data: mockData } })),
+}));
 
 describe("Analytics component", () => {
-  it("renders correct number of AnalyticsCard components ", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders correct number of AnalyticsCard components ", async () => {
+    instance.mockResolvedValueOnce({ data: { data: mockData } });
+
     render(<Analytics />);
 
-    const analytics = screen.getByTestId("analytics");
+    const analytics = await screen.findByTestId("analytics");
 
     expect(analytics).toBeInTheDocument();
-    ANALYTIC_CARDS.forEach((item) => {
-      const titleElement = screen.getByText(item.title);
-      expect(titleElement).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(analytics.children.length).toBe(mockData.length);
     });
   });
 });
--- packages/ui/src/components/analytics/Analytics.jsx
+++ packages/ui/src/components/analytics/Analytics.jsx
@@ -1,12 +1,31 @@
 import AnalyticsCard from "./AnalyticsCard";
-import { ANALYTIC_CARDS } from "../../utils/Constants";
 import styles from "./Analytics.module.css";
+import { useState, useEffect } from "react";
+import { useApi } from "../../hooks/useApi";
 
 function Analytics() {
+  const [stats, setStats] = useState([]);
+  const { makeRequest, data } = useApi({
+    method: "GET",
+    url: "/catalog/stats",
+  });
+
+  useEffect(() => {
+    makeRequest().catch((err) => {
+      console.error("Error fetching analytics data:", err);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setStats(data.data);
+    }
+  }, [data]);
+
   return (
     <div className={styles.analytics} data-testid="analytics">
-      {ANALYTIC_CARDS.map((item) => (
-        <AnalyticsCard key={item.title} title={item.title} api={item.api} />
+      {stats.map((item) => (
+        <AnalyticsCard key={item.title} title={item.title} api={item.value} />
       ))}
     </div>
   );
