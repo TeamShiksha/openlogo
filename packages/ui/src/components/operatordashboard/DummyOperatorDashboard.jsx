7d2b032716d6d4f0e7baddd13a0a6e0f3ad57574
sushanksinha <sushanksinha.5252@gmail.com>
2025-06-07 20:28:19 +0530

    update path for css file related to OPERATOR

--- packages/ui/src/components/operatordashboard/DummyOperatorDashboard.jsx
+++ packages/ui/src/components/operatordashboard/DummyOperatorDashboard.jsx
@@ -1,10 +1,10 @@
-import styles from "./admin/Admin.module.css";//mock css file //to be replaced with origin Operator.module.css file. 
+import styles from "./Operator.module.css";//mock css file //to be replaced with origin Operator.module.css file. 
 function OperatorDashboard() {
   return (
      //no need to add 'container class' along with styles in classname as it has been included inside parent div in Dashboard.jsx
      //Admin classname for css should be replaced with operator classname
      //data-testid="testid-operator-dashboard" is required for test cases 
-    <div className={styles["admin-page-container"]} data-testid="testid-operator-dashboard">
+    <div className={styles["operator-page-container"]} data-testid="testid-operator-dashboard">
       <p>Operator Dashboard - content to be replaced with the original implementation.</p>
     </div>
   );
