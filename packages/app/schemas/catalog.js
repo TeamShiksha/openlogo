8c137d96e440cfcc2714c62b98eabc4a1529364d
Aniket <aniketvishwakarma2004@gmail.com>
2025-08-11 14:49:23 +0530

    fix: logo api documentation mismatch

--- packages/app/schemas/catalog.js
+++ packages/app/schemas/catalog.js
@@ -1,47 +1,47 @@
 const Joi = require("joi");
 
 const getLogoQuerySchema = Joi.object({
-  domain: Joi.string()
+  key: Joi.string()
     .regex(/^[A-Za-z0-9&:.-/]+$/)
     .required()
     .messages({
-      "any.required": "Domain is required",
-      "string.pattern.base": "Invalid domain",
+      "any.required": "key is required",
+      "string.pattern.base": "Invalid key",
     }),
   API_KEY: Joi.string().guid({ version: "uuidv4" }).required().messages({
     "any.required": "API key is required",
   }),
 }).custom((value, helpers) => {
-  const { domain } = value;
-  const company = domain
+  const { key } = value;
+  const company = key
     .replace(/^(https?:\/\/)?(www\.)?/, "")
     .replace(/(\.[A-Za-z]{2,})+$/, "")
     .toUpperCase();
   if (company == "") {
-    return helpers.error("Domain cannot be empty");
+    return helpers.error("key cannot be empty");
   }
   return { ...value, company };
 });
 
 const getSearchQuerySchema = Joi.object({
-  domainKey: Joi.string()
+  key: Joi.string()
     .regex(/^[A-Za-z0-9&-/:.]+$/)
     .required()
     .messages({
-      "any.required": "domainKey is required",
-      "string.pattern.base": "Invalid domainKey",
+      "any.required": "key is required",
+      "string.pattern.base": "Invalid key",
     }),
   API_KEY: Joi.string().guid({ version: "uuidv4" }).required().messages({
     "any.required": "API key is required",
   }),
 }).custom((value, helpers) => {
-  const { domainKey } = value;
-  const companyNameBeginsWith = domainKey
+  const { key } = value;
+  const companyNameBeginsWith = key
     .replace(/^(https?:\/\/)?(www\.)?/, "")
     .match(/([A-Za-z0-9-]+)/)[1]
     .toUpperCase();
   if (companyNameBeginsWith === "") {
-    return helpers.error("domainKey cannot be empty");
+    return helpers.error("key cannot be empty");
   }
   return { ...value, companyNameBeginsWith };
 });
