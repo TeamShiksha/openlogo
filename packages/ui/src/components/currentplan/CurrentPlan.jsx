e98350f1ec2af81b2c6a068173e81e4a2358e3b5
printgourav <tiwary.gourav7@gmail.com>
2025-08-13 00:39:17 +0530

    Disables Non Functional Components Button

--- packages/ui/src/components/currentplan/CurrentPlan.jsx
+++ packages/ui/src/components/currentplan/CurrentPlan.jsx
@@ -1,19 +1,22 @@
 import { BUTTON_TEXT, CURRENT_PLAN } from "../../utils/Constants";
 import styles from "./CurrentPlan.module.css";
 import PropTypes from "prop-types";
+import Button from "../common/button/Button";
 
 function CurrentPlan({ isGuest }) {
+  const keepBtnDisabled = true;
   return (
     <>
       <p className={styles["current-plan"]}>{CURRENT_PLAN.plan}</p>
       <p className={styles["current-plan-tagline"]}>{CURRENT_PLAN.tagline}</p>
-      <button
+
+      <Button
         className={styles["upgrade-button"]}
-        disabled={isGuest}
+        disabled={keepBtnDisabled || isGuest}
         style={{ cursor: isGuest ? "default" : "pointer" }}
       >
         {BUTTON_TEXT.upgrade}
-      </button>
+      </Button>
     </>
   );
 }
