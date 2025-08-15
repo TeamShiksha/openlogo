a32d6e73a92659520ed9f00af24d166a4ffea464
Hariom Vashista <rishivashista4@gmail.com>
2025-08-15 22:05:30 +0530

    test(services): add unit tests for services (#773)
    * test(subscription): add unit tests for SubscriptionService methods
    
    * feat(services-test-case): refactor subscription service test case in one file
    - add test cases for userToken and keys
    
    * fix(test-coverage): add tests for image service
    
    * test(user-services): add test case for user services

---
+++ packages/app/__tests__/services/userTokens.js
@@ -0,0 +1,108 @@
+const UserToken = require("../../models/usertoken");
+const { UserTokenRepository } = require("../../repositories");
+const UserTokenService = require("../../services/usertoken");
+const { UserTokenTypes } = require("../../utils/constants");
+const { MOCK_USERTOKENS } = require("../../utils/mocks");
+
+describe("User Token Service", () => {
+  let userTokenService;
+
+  beforeEach(() => {
+    userTokenService = new UserTokenService();
+    jest.clearAllMocks();
+  });
+
+  it("create a new user token succesfully whose default value is VERIFY", async () => {
+    const userToken = new UserToken(MOCK_USERTOKENS[0]);
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "create")
+      .mockResolvedValue(userToken);
+
+    const result = await userTokenService.createUserToken(userToken.user_id);
+
+    expect(result).toBeDefined();
+    expect(result.type).toBe(UserTokenTypes.VERIFY);
+    expect(result.is_deleted).toBe(false);
+  });
+
+  it("should fetch a user token successfully for valid token", async () => {
+    const userToken = new UserToken(MOCK_USERTOKENS[0]);
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "fetchUserToken")
+      .mockResolvedValue(userToken);
+
+    const result = await userTokenService.fetchUserToken(userToken.token);
+
+    expect(result).toBeDefined();
+    expect(result._id).toBe(userToken._id);
+    expect(result.type).toBe(UserTokenTypes.VERIFY);
+  });
+
+  it("should return null if no user token found", async () => {
+    const userToken = "invalid-token";
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "fetchUserToken")
+      .mockResolvedValue(null);
+
+    const result = await userTokenService.fetchUserToken(userToken);
+
+    expect(result).toBe(null);
+  });
+
+  it("should fetch a deleted user token if valid token is provided", async () => {
+    const userToken = new UserToken(MOCK_USERTOKENS[4]);
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "fetchDeletedUserToken")
+      .mockResolvedValue(userToken);
+    const result = await userTokenService.fetchDeletedUserToken(
+      userToken.token
+    );
+
+    expect(result).toBeDefined();
+    expect(result.type).toBe(UserTokenTypes.VERIFY);
+    expect(result.is_deleted).toBe(true);
+  });
+
+  it("should return null if invalid token is provided", async () => {
+    const userToken = "invalid-token";
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "fetchDeletedUserToken")
+      .mockResolvedValue(null);
+
+    const result = await userTokenService.fetchDeletedUserToken(userToken);
+
+    expect(result).toBe(null);
+  });
+
+  it("should delete user token successfully", async () => {
+    const userToken = new UserToken(MOCK_USERTOKENS[0]);
+    const updatedTokenData = { ...userToken.toObject(), is_deleted: true };
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "update")
+      .mockResolvedValue(updatedTokenData);
+
+    const result = await userTokenService.deleteUserToken(userToken);
+    expect(result).toBeDefined();
+    expect(result.is_deleted).toBe(true);
+    expect(result.type).toBe(UserTokenTypes.VERIFY);
+  });
+
+  it("should create a forgot user token succesfully", async () => {
+    const userToken = new UserToken(MOCK_USERTOKENS[5]);
+
+    jest
+      .spyOn(UserTokenRepository.prototype, "create")
+      .mockResolvedValue(userToken);
+
+    const result = await userTokenService.createForgotToken(userToken.user_id);
+    expect(result).toBeDefined();
+    expect(result.type).toBe(UserTokenTypes.FORGOT);
+    expect(result.user_id).toBe("user@5");
+  });
+});
