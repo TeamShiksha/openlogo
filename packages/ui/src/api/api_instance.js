0e40ca66df68ac1081985bc80394ec7133873fd3
TanishqSingla <tanishqsingla00@gmail.com>
2025-08-21 20:56:17 +0530

    fix: nginx and docker compose setup

--- packages/ui/src/api/api_instance.js
+++ packages/ui/src/api/api_instance.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const VITE_BASE_URL = process.env.VITE_BASE_URL;
+
 export const instance = axios.create({
   baseURL: `${process.env.VITE_BASE_URL}/api`,
   headers: {
