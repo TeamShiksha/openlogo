d3dfc4c566ffecdf93750482c53ea2e327229cdf
TanishqSingla <tanishqsingla00@gmail.com>
2025-09-05 14:32:54 +0530

    fix: env not loading on vercel

--- packages/ui/src/api/api_instance.js
+++ packages/ui/src/api/api_instance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const VITE_BASE_URL = process.env.VITE_BASE_URL;
-
 export const instance = axios.create({
   baseURL: `${process.env.VITE_BASE_URL}/api`,
   headers: {
