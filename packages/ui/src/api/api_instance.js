19ab7ac6d9ce44d08fc3fa9e77321907eba4f6d5
Tanishq Singla <tanishqsingla00@gmail.com>
2025-09-18 22:35:27 +0530

    Fix dev setup (#841)
    * fix: nginx and docker compose setup
    
    * fix: env not loading on vercel
    
    * fix: use API_ prefix instead of VITE_

--- packages/ui/src/api/api_instance.js
+++ packages/ui/src/api/api_instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: `${process.env.VITE_BASE_URL}/api`,
+  baseURL: `${process.env.API_BASE_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
