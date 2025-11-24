f108df8bb2cd8185b86f99983406968167599c58
Sachin Kr. Singh~ <pramodkmr958@gmail.com>
2025-11-25 00:05:45 +0530

    feat: added documentation in development.md, made model schema,updated postman collection.

--- packages/app/routes/api_request.js
+++ packages/app/routes/api_request.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/auth");
 const { getApiStatsController } = require("../controllers/api_request");
+const { UserType } = require("../utils/constants");
 
-router.get("/stats", authMiddleware(), getApiStatsController);
+router.get(
+  "/stats",
+  authMiddleware({
+    roles: [UserType.ADMIN, UserType.OPERATOR, UserType.CUSTOMER],
+  }),
+  getApiStatsController
+);
 
 module.exports = router;
