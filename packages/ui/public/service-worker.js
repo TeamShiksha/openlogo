8b44be720340061e20b7ccb884f4addca84169fc
abhishek-2k23 <kumarabhishek00090@gmail.com>
2026-01-05 21:55:40 +0530

    fix prettier check

--- packages/ui/public/service-worker.js
+++ packages/ui/public/service-worker.js
@@ -1,10 +1,7 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html"
-      ]);
+      return cache.addAll(["/", "/index.html"]);
     })
   );
 });
