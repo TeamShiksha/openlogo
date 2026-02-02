0f688329d99d000622e7420232e3ba6b98117368
Mantu Kumar <mantu911711@gmail.com>
2026-02-02 15:53:22 +0530

    refactor: update validate-session tests to use sessionId

--- packages/app/__tests__/controllers/auth/validate-session.js
+++ packages/app/__tests__/controllers/auth/validate-session.js
@@ -2,20 +2,27 @@ const request = require("supertest");
 const app = require("../../../server");
 const { STATUS_CODES } = require("http");
 const { Users } = require("../../../models");
-const { MOCK_USERS } = require("../../../utils/mocks");
+const { UserSessionService } = require("../../../services");
+const {
+  MOCK_USERS,
+  MOCK_SESSION_ID,
+  MOCK_USER_SESSIONS,
+} = require("../../../utils/mocks");
 
 describe("GET /validate-session", () => {
   beforeAll(() => {
     process.env.CLIENT_URL = "https://localhost:3000";
-    process.env.JWT_SECRET = "secret";
   });
 
   afterAll(() => {
     delete process.env.CLIENT_URL;
-    delete process.env.JWT_SECRET;
   });
 
   it("401 - token does not exist", async () => {
+    jest
+      .spyOn(UserSessionService.prototype, "validateSession")
+      .mockResolvedValue(null);
+
     const response = await request(app).get("/api/auth/validate-session");
 
     expect(response.status).toBe(401);
@@ -26,22 +33,32 @@ describe("GET /validate-session", () => {
     });
   });
 
-  it.skip("200 - Success", async () => {
+  it("200 - Success", async () => {
     const mockUser = new Users(MOCK_USERS[1]);
-    const mockToken = mockUser.generateJWT();
+    const userData = mockUser.data();
+
+    jest
+      .spyOn(UserSessionService.prototype, "validateSession")
+      .mockResolvedValue({
+        ...MOCK_USER_SESSIONS[1],
+        userId: mockUser,
+      });
 
     const response = await request(app)
       .get("/api/auth/validate-session")
-      .set("Cookie", `jwt=${mockToken}`);
-
-    const userData = mockUser.data();
+      .set("Cookie", `sessionId=${MOCK_SESSION_ID}`);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
       statusCode: 200,
       userData: {
-        ...userData,
-        created_at: new Date(userData.created_at).toISOString(),
+        userId: userData.userId,
+        name: userData.name,
+        email: userData.email,
+        role: userData.role,
+        is_verified: userData.is_verified,
+        subscription_id: userData.subscription_id,
+        is_deleted: userData.is_deleted,
         updated_at: new Date(userData.updated_at).toISOString(),
       },
     });
